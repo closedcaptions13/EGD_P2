8fed0c9987e7de42349e28368faef8b2c586bab7
closedcaptions13 <closedcaptions13@gmail.com>
2025-07-11 01:51:31 -0400

    quiz prototype finished

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -2,28 +2,33 @@ using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
 
 public class QuizManager : MonoBehaviour
 {
-    // [SerializeField] private TMP_Text questionBox;
-    // [SerializeField] private TMP_Text answerBox1;
-    // [SerializeField] private TMP_Text answerBox2;
-    // [SerializeField] private TMP_Text answerBox3;
-    // [SerializeField] private TMP_Text answerBox4;
+    [SerializeField] private GameObject questionBox;
+    [SerializeField] private GameObject answerBox1;
+    [SerializeField] private GameObject answerBox2;
+    [SerializeField] private GameObject answerBox3;
+    [SerializeField] private GameObject answerBox4;
+    [SerializeField] private TMP_Text resultBox;
 
     public TextAsset questionsJson;
 
-    // private Dictionary<String, String> allQuestions;
+    private int num;
 
-    [System.Serializable]
+    private int finalScore;
+
+
+    [Serializable]
     public class Question
     {
-        public string question;
-        public string correctAnswer;
-        public string[] incorrectAnswers;
+        public string problem;
+        public string correct;
+        public string[] answers;
     }
 
-    [System.Serializable]
+    [Serializable]
     public class QuestionList
     {
         public Question[] question;
@@ -34,16 +39,51 @@ public class QuizManager : MonoBehaviour
     void Start()
     {
         questionList = JsonUtility.FromJson<QuestionList>(questionsJson.text);
+
+        finalScore = 0;
+        num = 0;
+        updateQuestion();
+
+        answerBox1.GetComponent<Button>().onClick.AddListener(() => chooseAnswer(answerBox1.GetComponent<Button>()));
+        answerBox2.GetComponent<Button>().onClick.AddListener(() => chooseAnswer(answerBox2.GetComponent<Button>()));
+        answerBox3.GetComponent<Button>().onClick.AddListener(() => chooseAnswer(answerBox3.GetComponent<Button>()));
+        answerBox4.GetComponent<Button>().onClick.AddListener(() => chooseAnswer(answerBox4.GetComponent<Button>()));
+    }
+    
+
+    private void updateQuestion()
+    {
+        questionBox.GetComponent<TMP_Text>().text = questionList.question[num].problem;
+        answerBox1.GetComponentInChildren<TextMeshProUGUI>().text = questionList.question[num].answers[0];
+        answerBox2.GetComponentInChildren<TextMeshProUGUI>().text = questionList.question[num].answers[1];
+        answerBox3.GetComponentInChildren<TextMeshProUGUI>().text = questionList.question[num].answers[2];
+        answerBox4.GetComponentInChildren<TextMeshProUGUI>().text = questionList.question[num].answers[3];
     }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     getNextQuestion();
-    // }
+    private void chooseAnswer(Button buttonNum)
+    {
+        if (buttonNum.GetComponentInChildren<TextMeshProUGUI>().text == questionList.question[num].correct)
+        {
+            finalScore++;
+        }
 
-    // private void getNextQuestion()
-    // {
+        num++; 
+        if (num < questionList.question.Length)
+        {
+            updateQuestion();
+        }
+        else if (num == questionList.question.Length)
+        {
+            questionBox.SetActive(false);
+            answerBox1.SetActive(false);
+            answerBox2.SetActive(false);
+            answerBox3.SetActive(false);
+            answerBox4.SetActive(false);
+
+            resultBox.text = "Final Score: " + finalScore + "/" + questionList.question.Length;
+        }
+
         
-    // }
+    }
+
 }
