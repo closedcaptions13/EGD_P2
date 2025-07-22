1e93385503ce9a5ebfc9cd88a4852f63ba2dceb1
sanlel <sanlel@rpi.edu>
2025-07-22 01:18:04 -0400

    I CODED SOMETHINGGGG
    yay

---
+++ Assets/PowerpointManager.cs
@@ -0,0 +1,86 @@
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PowerpointManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //set up image
+   
+    public RawImage displayslide;
+
+    private int currentSlide = 0;
+
+    //set up arrray 
+    public Texture[] Slides = new Texture[18];
+
+
+    void Start()
+    {
+
+        displayslide.texture = Slides[currentSlide];
+        
+    }
+
+    /*public void NextButton()
+    {
+        //currentSlide++;
+
+        if (currentSlide > Slides.Length - 1)
+        {
+            currentSlide = 0;
+        }
+
+        Debug.Log(Slides[currentSlide]);
+        displayslide.texture = Slides[currentSlide];
+    }*/
+
+/*public void PreviousButton()
+{
+    //currentSlide--;
+
+    if (currentSlide < 0)
+    {
+        currentSlide = Slides.Length -1;
+    }
+
+    Debug.Log(Slides[currentSlide]);
+        displayslide.texture = Slides[currentSlide];
+    }*/
+
+
+
+// Update is called once per frame
+void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || (Input.GetKeyDown(KeyCode.DownArrow)))
+        {
+            currentSlide++;
+
+            if (currentSlide > Slides.Length - 1)
+            {
+                currentSlide = 0;
+            }
+
+            Debug.Log(Slides[currentSlide]);
+            displayslide.texture = Slides[currentSlide];
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            currentSlide--;
+
+            if (currentSlide < 0)
+            {
+                currentSlide = Slides.Length - 1;
+            }
+
+            Debug.Log(Slides[currentSlide]);
+            displayslide.texture = Slides[currentSlide];
+        }
+    }
+}
